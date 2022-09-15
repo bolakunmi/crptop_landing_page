51d65402a1c7d26de1f0023aca5b4535823215d2
Bolakunmi <boluwayemi2000@gmail.com>
2022-09-16 00:30:34 +0100

    responsiveness done, media query still unsettled

--- src/Body.js
+++ src/Body.js
@@ -40,7 +40,39 @@ const BODY = () => {
             src={require("./files/hero-ribbon-2.png")}
             alt="background-img"
           />
-        </section>:<loadig/>)}
+        </section>:<section className="hero-section blur">
+          <div className="hero-description-container">
+            <div className="hero-advert">
+              <div>70% save</div>
+              <div>for the best black friday deals</div>
+            </div>
+
+            <h1>fastest & secure platform to invest in cypto</h1>
+            <p>
+              buy and sell cyptocurrencies by trusted 10M wallets, in over 100m
+              transactions
+            </p>
+            <button>
+              try for FREE
+              <div className="vector">
+                <img src={require("./files/Vector.png")} alt="arrow" />
+              </div>
+            </button>
+          </div>
+          <div>
+            <img src={require("./files/hero-img.png")} alt="hero-img" />
+          </div>
+          <img
+            className="background background-1"
+            src={require("./files/hero-ribbon-1.png")}
+            alt="background-img"
+          />
+          <img
+            className="background background-2"
+            src={require("./files/hero-ribbon-2.png")}
+            alt="background-img"
+          />
+        </section>)}
         </TrackVisibility>
         
 <TrackVisibility partialVisibility>
@@ -81,7 +113,44 @@ const BODY = () => {
               <p>trusted wallet investors</p>
             </div>
           </div>
-        </section>:<loading/>)}
+        </section>:<section className="info blur">
+          <div className="info-div">
+            <div className="info-div-img">
+              <img
+                src={require("./files/imports/Bar.png")}
+                alt="hero-img"
+              ></img>
+            </div>
+            <div>
+              <h2>$30B</h2>
+              <p>digital currency exchange</p>
+            </div>
+          </div>
+          <div className="info-div">
+            <div className="info-div-img">
+              <img
+                src={require("./files/imports/Person.png")}
+                alt="hero-img"
+              ></img>
+            </div>
+            <div>
+              <h2>190+</h2>
+              <p>countries around the world</p>
+            </div>
+          </div>
+          <div className="info-div">
+            <div className="info-div-img">
+              <img
+                src={require("./files/imports/Earth.png")}
+                alt="hero-img"
+              ></img>
+            </div>
+            <div>
+              <h2>$10M</h2>
+              <p>trusted wallet investors</p>
+            </div>
+          </div>
+        </section>)}
         </TrackVisibility>
         
 <TrackVisibility partialVisibility>
@@ -111,7 +180,33 @@ const BODY = () => {
             </p>
             <button>learn more</button>
           </div>
-        </section>:<loading/>)}
+        </section>:<section className="blur">
+          <img
+            className="background background-3"
+            src={require("./files/why-model-1.png")}
+            alt="background-img"
+          />
+          <img
+            className="background background-4"
+            src={require("./files/why-ribbon.png")}
+            alt="background-img"
+          />
+          <img
+            className="background background-5"
+            src={require("./files/why-model-2.png")}
+            alt="background-img"
+          />
+          <img src={require("./files/why-img.png")} alt="why-img" />
+          <div>
+            <h1>why you should choose CHAPPO</h1>{" "}
+            <p style={{ "font-size": "16px" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+              laboriosam? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. In, praesentium.
+            </p>
+            <button>learn more</button>
+          </div>
+        </section>)}
         </TrackVisibility>
 
         <TrackVisibility partialVisibility>
@@ -157,7 +252,49 @@ const BODY = () => {
               </p>
             </div>
           </div>
-        </section>:<loading/>)}
+        </section>:<section className="blur">
+          <div className="blur">
+            <h2>Check how much you can earn with us</h2>
+            <p>
+              lets check your hash rate to see how much you will earn today,
+              <br></br> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+
+          <div className="calculator-container">
+            <form className="calculator-tools">
+              <input placeholder="Enter your hash rate"></input>
+
+              <select>
+                <option>TH/s</option>
+                <option>H/s</option>
+                <option>KH/s</option>
+                <option>GH/s</option>
+                <option>MH/s</option>
+              </select>
+              <button>calculate</button>
+            </form>
+            <div className="calculator-info">
+              <h2
+                style={{
+                  "font-size": "16px",
+                  color: "#3671E9",
+                  marginBottom: "16px",
+                }}
+              >
+                ESTIMATED 24 HOUR REVENUE:
+              </h2>
+              <h2 style={{ "font-size": "32px", marginBottom: "4px" }}>
+                0.055 130 59 ETH{" "}
+                <span style={{ color: "#3671E9" }}>($1275)</span>
+              </h2>
+              <p>
+                Revenue will change based on mining difficulty and Ethereum
+                price.
+              </p>
+            </div>
+          </div>
+        </section>)}
         </TrackVisibility>
         
         
--- src/Body2.js
+++ src/Body2.js
@@ -6,74 +6,139 @@ const BODY2 = () => {
     <React.Fragment>
       <section>
         <TrackVisibility partialVisibility>
-            {({ isVisible }) => ( isVisible? <div className="white-section">
-          <h1 style={{ textAlign: "center" }}>
-            Trade securely and market the high growth cryptocurrencies.
-          </h1>
-          <div className="coins-container">
-            <div className="coins">
-              <img src={require("./files/bitcon.png")} alt="Bitcoin" />
-              <div className="coins-description">
-                <h2>Bitcoin</h2>
-                <p>BTC</p>
-              </div>
-              <p>
-                Digital currency in which a record of transactions is
-                maintained.
-              </p>
-              <button className="coins-button">
-                <p>Start mining</p>
-                <div className="vector2">
-                  <img src={require("./files/Vector.png")} alt="arrow" />
+          {({ isVisible }) =>
+            isVisible ? (
+              <div className="white-section">
+                <h1 style={{ textAlign: "center" }}>
+                  Trade securely and market the high growth cryptocurrencies.
+                </h1>
+                <div className="coins-container">
+                  <div className="coins">
+                    <img src={require("./files/bitcon.png")} alt="Bitcoin" />
+                    <div className="coins-description">
+                      <h2>Bitcoin</h2>
+                      <p>BTC</p>
+                    </div>
+                    <p>
+                      Digital currency in which a record of transactions is
+                      maintained.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
+
+                  <div className="coins">
+                    <img src={require("./files/ethereum.png")} alt="ethereum" />
+                    <div className="coins-description">
+                      <h2>Ethereum</h2>
+                      <p>ETH</p>
+                    </div>
+                    <p>
+                      Blockchain technology to create and run decentralized
+                      digital applications.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
+
+                  <div className="coins">
+                    <img src={require("./files/litecoin.png")} alt="litecoin" />
+                    <div className="coins-description">
+                      <h2>Litecoin</h2>
+                      <p>LTC</p>
+                    </div>
+                    <p>
+                      Cryptocurrency that enables instant payments to anyone in
+                      the world.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
                 </div>
-              </button>
-            </div>
-
-            <div className="coins">
-              <img src={require("./files/ethereum.png")} alt="ethereum" />
-              <div className="coins-description">
-                <h2>Ethereum</h2>
-                <p>ETH</p>
               </div>
-              <p>
-                Blockchain technology to create and run decentralized digital
-                applications.
-              </p>
-              <button className="coins-button">
-                <p>Start mining</p>
+            ) : (
+              <div className="blur" >
+                <h1 style={{ textAlign: "center" }}>
+                  Trade securely and market the high growth cryptocurrencies.
+                </h1>
+                <div className="coins-container">
+                  <div className="coins">
+                    <img src={require("./files/bitcon.png")} alt="Bitcoin" />
+                    <div className="coins-description">
+                      <h2>Bitcoin</h2>
+                      <p>BTC</p>
+                    </div>
+                    <p>
+                      Digital currency in which a record of transactions is
+                      maintained.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
 
-                <div className="vector2">
-                  <img src={require("./files/Vector.png")} alt="arrow" />
+                  <div className="coins">
+                    <img src={require("./files/ethereum.png")} alt="ethereum" />
+                    <div className="coins-description">
+                      <h2>Ethereum</h2>
+                      <p>ETH</p>
+                    </div>
+                    <p>
+                      Blockchain technology to create and run decentralized
+                      digital applications.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
+
+                  <div className="coins">
+                    <img src={require("./files/litecoin.png")} alt="litecoin" />
+                    <div className="coins-description">
+                      <h2>Litecoin</h2>
+                      <p>LTC</p>
+                    </div>
+                    <p>
+                      Cryptocurrency that enables instant payments to anyone in
+                      the world.
+                    </p>
+                    <button className="coins-button">
+                      <p>Start mining</p>
+
+                      <div className="vector2">
+                        <img src={require("./files/Vector.png")} alt="arrow" />
+                      </div>
+                    </button>
+                  </div>
                 </div>
-              </button>
-            </div>
-
-            <div className="coins">
-              <img src={require("./files/litecoin.png")} alt="litecoin" />
-              <div className="coins-description">
-                <h2>Litecoin</h2>
-                <p>LTC</p>
               </div>
-              <p>
-                Cryptocurrency that enables instant payments to anyone in the
-                world.
-              </p>
-              <button className="coins-button">
-                <p>Start mining</p>
-
-                <div className="vector2">
-                  <img src={require("./files/Vector.png")} alt="arrow" />
-                </div>
-              </button>
-            </div>
-          </div>
-        </div>: <loading/>)}
+            )
+          }
         </TrackVisibility>
-        
       </section>
 
-      <TrackVisibility partialVisibility>
-            {({ isVisible }) => (isVisible? <section className="statistics">
+      <section className="statistics">
         <img
           className="background background-6"
           src={require("./files/features-circle-1.png")}
@@ -84,102 +149,171 @@ const BODY2 = () => {
           choice
         </h1>
         <div className="statistics-container">
-          <div className="statistics-object">
-            <div>
-              <h2>Invest Smart</h2>
-              <p>
-                Get full statistic information about the behaviour of buyers and
-                sellers will help you to make the decision.
-              </p>
-              <button>learn more</button>
-            </div>
-            <img
-              src={require("./files/feature-1-img.png")}
-              alt="feature-1-img.png"
-            />
-          </div>
+          <TrackVisibility partialVisibility>
+            {({ isVisible }) =>
+              isVisible ? (
+                <div className="statistics-object">
+                  <div>
+                    <h2>Invest Smart</h2>
+                    <p>
+                      Get full statistic information about the behaviour of
+                      buyers and sellers will help you to make the decision.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-1-img.png")}
+                    alt="feature-1-img.png"
+                  />
+                </div>
+              ) : (
+                <div className=" blur">
+                  <div>
+                    <h2>Invest Smart</h2>
+                    <p>
+                      Get full statistic information about the behaviour of
+                      buyers and sellers will help you to make the decision.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-1-img.png")}
+                    alt="feature-1-img.png"
+                  />
+                </div>
+              )
+            }
+          </TrackVisibility>
 
-          <div className="statistics-object">
-            <div>
-              <h2>Detailed Statistics</h2>
-              <p>
-                View all mining related information in realtime, at any point at
-                any location and decide which polls you want to mine in.
-              </p>
-              <button>learn more</button>
-            </div>
-            <img
-              src={require("./files/feature-2-img.png")}
-              alt="feature-2-img.png"
-            />
-          </div>
+          <TrackVisibility partialVisibility>
+            {({ isVisible }) =>
+              isVisible ? (
+                <div className="statistics-object">
+                  <div>
+                    <h2>Detailed Statistics</h2>
+                    <p>
+                      View all mining related information in realtime, at any
+                      point at any location and decide which polls you want to
+                      mine in.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-2-img.png")}
+                    alt="feature-2-img.png"
+                  />
+                </div>
+              ) : (
+                <div className=" blur">
+                  <div>
+                    <h2>Detailed Statistics</h2>
+                    <p>
+                      View all mining related information in realtime, at any
+                      point at any location and decide which polls you want to
+                      mine in.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-2-img.png")}
+                    alt="feature-2-img.png"
+                  />
+                </div>
+              )
+            }
+          </TrackVisibility>
 
-          <div className="statistics-object">
-            <div>
-              <h2>Grow your profit and track your investments</h2>
-              <p>
-                Use advanced analytical tools. Clear TradingView charts let you
-                track current and historical profit investments.
-              </p>
-              <button>learn more</button>
-            </div>
-            <img
-              src={require("./files/feature-3-img.png")}
-              alt="feature-3-img.png"
-            />
-          </div>
+          <TrackVisibility partialVisibility>
+            {({ isVisible }) =>(
+              isVisible ? (
+                <div className="statistics-object">
+                  <div>
+                    <h2>Grow your profit and track your investments</h2>
+                    <p>
+                      Use advanced analytical tools. Clear TradingView charts
+                      let you track current and historical profit investments.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-3-img.png")}
+                    alt="feature-3-img.png"
+                  />
+                </div>
+              ) : (
+                <div className="blur">
+                  <div>
+                    <h2>Grow your profit and track your investments</h2>
+                    <p>
+                      Use advanced analytical tools. Clear TradingView charts
+                      let you track current and historical profit investments.
+                    </p>
+                    <button>learn more</button>
+                  </div>
+                  <img
+                    src={require("./files/feature-3-img.png")}
+                    alt="feature-3-img.png"
+                  />
+                </div>
+              ))
+            }
+          </TrackVisibility>
         </div>
+      </section>
 
-      </section>:<loading/>)}
-        </TrackVisibility>
-      
-<TrackVisibility partialVisibility>
-            {({ isVisible }) => (isVisible? <section className="mining-section">
-                <img
-          className="background background-7"
-          src={require("./files/features-circle-2.png")}
-          alt="background-img"
-        />
-        <div className="subscribe-section">
-          <div className="subscribe-description">
-            <img
-              className="background background-8"
-              src={require("./files/ethereum-backgound.png")}
-              alt="background-img"
-            />
-            <h1>Start mining now</h1>
-            <p>
-              Join now with CRAPPO to get the latest news and start mining now
-            </p>
-          </div>
-          <div className="subscribe">
-            <input
-              placeholder="Enter your Email"
-              type="email"
-              id="email"
-              pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$"
-            ></input>
-            <button> Subscribe</button>
-            <img
-              className="background background-9"
-              src={require("./files/bitcoin-background.png")}
-              alt="background-img"
-            />
-          </div>
-        </div>
-        <img
-          className="background background-10"
-          src={require("./files/something2.png")}
-          alt="background-img"
-        />
-        <img
-          className="background background-11"
-          src={require("./files/something.png")}
-          alt="background-img"
-        />
-      </section>:<loading/>)}
-        </TrackVisibility>
-      
+      <TrackVisibility partialVisibility>
+        {({ isVisible }) =>
+          isVisible ? (
+            <section className="mining-section">
+              <img
+                className="background background-7"
+                src={require("./files/features-circle-2.png")}
+                alt="background-img"
+              />
+              <div className="subscribe-section">
+                <div className="subscribe-description">
+                  <img
+                    className="background background-8"
+                    src={require("./files/ethereum-backgound.png")}
+                    alt="background-img"
+                  />
+                  <h1>Start mining now</h1>
+                  <p>
+                    Join now with CRAPPO to get the latest news and start mining
+                    now
+                  </p>
+                </div>
+                <div className="subscribe">
+                  <input
+                    placeholder="Enter your Email"
+                    type="email"
+                    id="email"
+                    pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$"
+                  ></input>
+                  <button> Subscribe</button>
+                  <img
+                    className="background background-9"
+                    src={require("./files/bitcoin-background.png")}
+                    alt="background-img"
+                  />
+                </div>
+              </div>
+              <img
+                className="background background-10"
+                src={require("./files/something2.png")}
+                alt="background-img"
+              />
+              <img
+                className="background background-11"
+                src={require("./files/something.png")}
+                alt="background-img"
+              />
+            </section>
+          ) : (
+            <loading />
+          )
+        }
+      </TrackVisibility>
     </React.Fragment>
   );
 };
